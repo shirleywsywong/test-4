40534be73edc7edbb7b283518cdd3c33aef1b2fd
Shirley Wong <shirleywsywong@gmail.com>
2019-10-01 19:58:22 -0400

    some error handling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, {Component} from 'react';
 import Form from './components/Form';
+import './App.css';
 
 class App extends Component {
+
+  
   render(){
     
     return (
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
+    constructor() {
+        super();
+        this.state = {
+            file: {},
+            fileName: '',
+            fileType: '',
+        }
+    }
+    onFormSubmit = (e) => {
+        e.preventDefault();
+        const selectedFile = document.getElementById('file').files[0];
+        
+        console.log(selectedFile);
+        if (selectedFile == undefined) {
+            alert(`Please upload a CSV file.`);
+            this.render();
+        }
+        
+        //check for valid file type
+        let fileName = selectedFile.name;
+        let fileType = fileName.substr(fileName.length - 4);
+        if (fileType != '.csv') {
+            alert(`Invalid file type. Please upload a CSV file.`)
+        }
+    }
+
     render() {
         return (
-            <form action="post">
-                <div><label htmlFor="file">Please upload a CSV file</label></div>
-                <div><input type="file" name="file" id="file" accept=".csv"/></div>
+            <form action="" onSubmit={this.onFormSubmit}>
+                <div>
+                    <label htmlFor="file">Please upload a CSV file</label>
+                </div>
+                <div>
+                    <input 
+                        type="file" 
+                        name="file" 
+                        id="file" 
+                        accept=".csv"/>
+                </div>
                 <button>Upload</button>
             </form>
         );
