cabcc8bdd21f6a5187d85377c7bc3c1e23e78991
Shirley Wong <shirleywsywong@gmail.com>
2019-10-01 22:50:03 -0400

    rendering csv data on console

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,27 +5,68 @@ class Form extends Component {
         super();
         this.state = {
             file: {},
-            fileName: '',
-            fileType: '',
+            fileContentCSV: '',
         }
     }
+
+    //once form submits, set the file into state
     onFormSubmit = (e) => {
         e.preventDefault();
         const selectedFile = document.getElementById('file').files[0];
-        
         console.log(selectedFile);
-        if (selectedFile == undefined) {
-            alert(`Please upload a CSV file.`);
-            this.render();
-        }
-        
-        //check for valid file type
-        let fileName = selectedFile.name;
-        let fileType = fileName.substr(fileName.length - 4);
-        if (fileType != '.csv') {
-            alert(`Invalid file type. Please upload a CSV file.`)
+
+        this.setState({
+            file: selectedFile,
+        });
+
+    }
+
+    //each time a new file in loaded, check for valid file type
+    componentDidUpdate(prevProps, prevState) {
+        const { file } = this.state;
+
+        if (file !== prevState.file) {
+            this.checkFile();
         }
     }
+    
+    //check if user uploaded a file, and valid file type
+    checkFile = () => {
+
+        //if user did not upload a file
+        if (this.state.file === undefined) {
+            alert(`Please upload a CSV file.`);
+        } else {
+
+            //check if user uploaded a csv file
+            let fileName = this.state.file.name;
+            let fileType = fileName.substr(fileName.length - 4);
+            console.log(`check file type`)
+            if (fileType !== '.csv') {
+                alert(`Invalid file type. Please upload a CSV file.`)
+            } else {
+
+                //open the file
+                this.readFile();
+            }
+        }
+    }
+
+    //open the file with the FileReader object
+    readFile = () => {
+        const reader = new FileReader();
+        reader.onload = function(event) {
+            let contents = event.target.result;
+            this.setState({fileContentCSV: contents});
+        };
+
+        // reader.onerror = function(event) {
+        //     console.error("File could not be read! Code " + event.target.error.code);
+        // };
+
+        reader.readAsText(this.state.file);
+    }
+    
 
     render() {
         return (
