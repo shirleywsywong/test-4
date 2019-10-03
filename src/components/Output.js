34ff130fd53395c58163a7d9c19345c8c2bf49a7
Shirley Wong <shirleywsywong@gmail.com>
2019-10-03 15:32:30 -0400

    �� got csv into state

--- src/components/Output.js
+++ src/components/Output.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Papa from 'papaparse';
 
 class Output extends Component {
     constructor() {
@@ -7,35 +8,68 @@ class Output extends Component {
         }
     }
 
-    //each time a new prop comes in, run the separate values function
+    // each time a new prop comes in, run the separate values function
     componentDidUpdate(prevProps, prevState) {
-        this.separateVal();
+
+        if (this.props.rawCSV !== prevProps.rawCSV) {
+            this.separateVal();
+        }
+
     }
     
     //split the csv into individual array 
     separateVal = () => {
 
-        let CSVarrays = this.props.rawCSV.split('\r\n');
-        
-        let dataArrays = CSVarrays.map(function(val) {
-            return val.split(',')
-        })
-        console.log(dataArrays); 
+        //get only the headings
+        let CSVheadingArr = Papa.parse(this.props.rawCSV)
+        let CSVheading = CSVheadingArr.data[0]
+        console.log(CSVheading)
 
-        // this.setState({
-        //     CSVarrays: CSVarrays,
-        // })
+        //put the values in an object with the headings as key
+        let CSVObj = Papa.parse(this.props.rawCSV, {header: true})
+        let dataArr = CSVObj.data
+        console.log(dataArr);
+
+        this.setState({
+            heading: CSVheading,
+            data: dataArr
+        })
+
+        
+    }
+
+    renderingVal = () => {
+
+        //display data in a grid
+        //heading row with headings
+        //put each value of the object into a grid element
+        
+        // return(
+        //     <div className="output-grid">
+        //         {CSVheading.map((heading) => {
+        //             return <div className="heading">{heading}</div>
+        //         })}
+        //     </div>
+        // )
     }
 
     render() {
         return(
             <section>
-                <p>
-                    {this.props.rawCSV}
-                </p>
+                {/* {this.separateVal()} */}
             </section>
         )
     }
 }
 
 export default Output;
+
+// componentDidUpdate() {
+//     setStatefn();
+// }
+
+// setStatefn = () => {
+//     this.setState({
+//         data: this.props.data
+//     })
+// }
